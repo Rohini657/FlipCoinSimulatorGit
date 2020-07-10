94e4b9aeac8bba2d210464cb7a5dbdb772e71b1f
Rohini657 <rohinijadhav3196@gmail.com>
2020-07-10 17:29:18 +0530

    Calculation Added

--- FlipCoin.sh
+++ FlipCoin.sh
@@ -10,4 +10,26 @@ else
 	echo "Tail Wins"
 fi
 
-
+####Use Case2
+read -p "Enter No Of Flip : " $n
+declare -A dic
+dic[HEAD]=0;
+dic[TAIL]=0;
+for (( i=0;i<=$n++ ))
+do
+	r=$((RANDOM%2))
+	if [ $r -eq 0 ]
+	then
+		val=${dic[HEAD]}
+		dic[HEAD]=$((val+1))
+	else
+		val=${dic[TAIL]}
+		dic[TAIL]=$((val+1))
+	fi
+done
+val=${dic[HEAD]}
+echo "Percent Of Head Is:="
+echo `Scale=2; $val/$n*100`
+val=${dic[TAIL]}
+echo "Percent Of Tail Is:="
+echo `Scale=2; $val/$n*100`
