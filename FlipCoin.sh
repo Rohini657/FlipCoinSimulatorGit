69cddf5df6d9c31b2ecc3da32920b8c9296668d4
Rohini657 <rohinijadhav3196@gmail.com>
2020-07-10 17:41:12 +0530

    Simulator Update

--- FlipCoin.sh
+++ FlipCoin.sh
@@ -33,3 +33,29 @@ echo `Scale=2; $val/$n*100`
 val=${dic[TAIL]}
 echo "Percent Of Tail Is:="
 echo `Scale=2; $val/$n*100`
+
+####Use Case3
+#loop For Simulator
+hwin=0
+twin=0
+while [ $hwin -lt 21 ] && [ $twin -lt 21 ]
+do
+	toss=$((RANDOM%2))
+	if [ $toss -eq 1 ]
+	then
+		echo "HEAD"
+		((hwin++))
+	else
+		echo "TAILS"
+		((twin++))
+	fi
+done
+echo "NO OF TIME HEAD WIN"$hwin
+echo "NO OF TIME TAIL WIN"$twin
+
+if [ $hwin -gt $twin ]
+then
+echo "HEAD WIN BY"$((hwin-twin))
+else
+echo "TAILS WIN BY"$((hwin-twin))
+fi
