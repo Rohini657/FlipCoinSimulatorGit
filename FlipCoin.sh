a04c3c711864b7a142e455d69657932abebc58ea
Rohini657 <rohinijadhav3196@gmail.com>
2020-07-10 17:44:41 +0530

    21 Times

--- FlipCoin.sh
+++ FlipCoin.sh
@@ -59,3 +59,29 @@ echo "HEAD WIN BY"$((hwin-twin))
 else
 echo "TAILS WIN BY"$((hwin-twin))
 fi
+
+####Use Case4
+##21 Times Won
+hwin=0
+twin=0
+while [ $hwin -lt 21 ] && [ $twin -lt 21 ]
+do
+	toss=$((RANDOM%2))
+	if [ $toss -eq 1 ]
+	then
+		echo "HEAD"
+		((hwin++))
+	else
+		echo "TAILS"
+		((twin++))
+	fi
+done
+echo "NO OF TIME HEAD WIN"$hwin
+echo "NO OF TIME TAIL WIN"$twin
+
+if [ $hwin -gt $twin ]
+then
+echo "HEAD WIN BY"$((hwin-twin))
+else
+echo "TAILS WIN BY"$((hwin-twin))
+fi
